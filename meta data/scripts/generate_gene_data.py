fc7394c275595c62d3c2efcfbaca99e08445ee45
PragyaJaiswal <pragya.jswl@gmail.com>
2016-01-28 12:25:50 +0530

    Generated Gene map with hosts and targets.

--- meta data/scripts/generate_gene_data.py
+++ meta data/scripts/generate_gene_data.py
@@ -22,11 +22,18 @@ class restructure_data(object):
 		restructure_data.append_host_target_mirna(gene_map)
 
 	def append_host_target_mirna(gene_map):
+		'''
+		gene_data_new contains the host as well as target genes as keys.
+		Total number of keys are 15112.
+		Hence, it has more than 14894 genes since, 14894 are the target
+		genes (from mirTarBase) and the remaining are the host genes by
+		coordinate matching of ensembl genes and mirna coordinates.
+		'''
 		with open('../output_data/mirna/mirna_meta_data_complete.json') as infile:
 			miRNA_meta_data = json.loads(infile.read())
 			gene_data_new = {}
 			for mirna in miRNA_meta_data.keys():
-				if 'Host Gene' in miRNA_meta_data[mirna].keys() and not miRNA_meta_data[mirna]['Host Gene'] == '':
+				if 'Host Gene' in miRNA_meta_data[mirna].keys():
 					gene = miRNA_meta_data[mirna]['Host Gene']
 					
 					gene_data_new.setdefault(gene, {})
@@ -37,16 +44,23 @@ class restructure_data(object):
 					else:
 						gene_data_new[gene]['Target for'] = ''
 
+			for key, value in gene_map.items():
+				gene_data_new.setdefault(key, {})
+				gene_data_new[key]['Target for'] = value
+
 			print(len(gene_data_new.keys()))
+			jsonify(gene_data_new, '../output_data/gene/gene_meta_data.json')
+
 
 def jsonify(dictionary, filename, text='None'):
-	a = json.dumps(dictionary, sort_keys=True, indent=4, separators=(',', ': '))
+	a = json.dumps(dictionary, sort_keys=True, indent=2, separators=(',', ': '))
 	with open(str(filename), 'w') as outfile:
 		if text == 'None':
 			outfile.write(a)
 		else:
 			outfile.write(text + ' = ')
 			outfile.write(a)
+
 
 if __name__ == '__main__':
 	instance = restructure_data()
